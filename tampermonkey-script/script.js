50b68053afde5caa26877f042aaf117df5150a75
Elanis <elanis@hotmail.com>
2025-08-31 19:49:39 +0200

    Create userscript (previous version was the GRDF one)

--- tampermonkey-script/script.js
+++ tampermonkey-script/script.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         Enedis LUMEN
 // @namespace    http://elanis.eu/
-// @version      2025-08-07
+// @version      2025-08-31
 // @description  Sends the current session cookie to the Lumen API's Enedis Module, so it can query consumption data and save it
 // @author       Axel "Elanis" Soupé
-// @match        https://monespace.grdf.fr/client/particulier/consommation
+// @match        https://mon-compte-particulier.enedis.fr/home-connectee
 // @icon         icon
 // @grant        GM.cookie
 // @grant        GM_xmlhttpRequest
 // @homepageURL  https://github.com/L-U-M-E-N/Lumen.Modules.Enedis
 // @connect      lumen.domain.tld
+// @include https://alex.microapplications.enedis.fr/
 // ==/UserScript==
 
 ////////////////
@@ -21,22 +22,25 @@ const LUMEN_SERVER_API_KEY = 'FILLME';
 //// Constants
 const LUMEN_SERVER_AUTH_HEADER_NAME = 'X-Api-Key';
 const LUMEN_SERVER_ROUTE = 'EnedisData/queryDataFromEnedis';
-const Enedis_URL = 'https://monespace.grdf.fr/client/particulier/consommation';
+const Enedis_URL = 'https://alex.microapplications.enedis.fr/';
 
 //// Execution
 const cs = await GM.cookie.list({ url: Enedis_URL, partitionKey: {} });
 const formattedCookie = cs.map((c) => c.name + '=' + c.value).join('; ');
-GM.xmlHttpRequest({
-  url: LUMEN_SERVER_URL + LUMEN_SERVER_ROUTE,
-  method: "POST",
-  data: JSON.stringify({ cookie: formattedCookie }),
-  headers: {
-    "Content-Type": "application/json",
-    [LUMEN_SERVER_AUTH_HEADER_NAME]: LUMEN_SERVER_API_KEY
-  },
-}).then((res) => {
-  console.log(res);
-  if (res.status === 202) {
-    alert('Enedis cookie has been submitted to LUMEN server and data has been updated.');
-  }
-}).catch(e => console.error(e));
+
+setTimeout(() => {
+  GM.xmlHttpRequest({
+    url: LUMEN_SERVER_URL + LUMEN_SERVER_ROUTE,
+    method: "POST",
+    data: JSON.stringify({ cookie: formattedCookie }),
+    headers: {
+      "Content-Type": "application/json",
+      [LUMEN_SERVER_AUTH_HEADER_NAME]: LUMEN_SERVER_API_KEY
+    },
+  }).then((res) => {
+    console.log(res);
+    if (res.status === 202) {
+      alert('Enedis cookie has been submitted to LUMEN server and data has been updated.');
+    }
+  }).catch(e => console.error(e));
+}, 5000);
